82ee92def15e10f423e7dda1c43075713cc05039
Jaqueline Rocha <jaquerotero@gmail.com>
2022-08-30 22:06:04 -0300

    segunda aula e lista carrinho de compras

---
+++ carrinho_compras.py
@@ -0,0 +1,26 @@
+cart = []
+# for i in range(3)
+
+id_user = input('Insira o id do usuário')
+id_produto = input('Insira o id do produto')
+price_product = float(input('Insira o preço do produto'))
+quantity_product = int(input('Insira a quantidade de produtos'))
+
+item = [id_user, id_produto, price_product, quantity_product]
+
+#Criar função para adicionar item no carrinho
+
+def add_item_cart(item):
+    pass
+
+# retorne todos os itens do carrinho
+def get_all_items_cart():
+    pass
+
+# trazer todos os itens do carrinho pelos itens do produto
+def get_item_card_by_id(id_product):
+    pass
+
+#remover o item do carrinho que tem esse produto
+def remove_item_id(id_product):
+    pass
---
+++ segundaaula.py
@@ -0,0 +1,28 @@
+# valor_energia = float(input("Informe o valor da conta de energia: "))
+# print(type(valor_energia))
+
+# qtdade_moradores = input("Quantos moradores: ")
+# qtdade_moradores = int(qtdade_moradores)
+
+# print(type(qtdade_moradores))
+
+# valor_cada_morador = valor_energia / qtdade_moradores
+# print(valor_cada_morador)
+
+
+# Remover apneas os inteiros da lista
+lista = ['banana', 'caju', 'melão', 5, 'pêra', 10]
+
+# list_new = []
+
+# for elemento in lista:
+#     if type(elemento) != int:
+#         lista.pop()
+#     print(index)
+
+
+# lista = [0, 5, 8, 10, 35, 5, 7, 4, 12, 22, 3, 2, 9, 1]
+# list_append[]
+
+# for i in lista:
+#     list_append.append(i + 1)
